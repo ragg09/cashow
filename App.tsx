61c81090ab5b6db240ff409f484c347f05aad858
ragg09 <54803732+ragg09@users.noreply.github.com>
2023-10-27 11:12:43 +0800

    Project Initial Configuration (#1)

--- App.tsx
+++ App.tsx
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import CounterScreen from './screens/counterScreen';
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      {/* <View style={styles.container}>
+        <Text>Counter</Text>
+        <Text>{count} asd</Text>
+        <StatusBar style="auto" />
+      </View> */}
+      <CounterScreen/>
+    </Provider>
+
   );
 }
 
