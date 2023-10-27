b1fed15d2c83dd43eab682a5b2489e8941240bab
ragg09 <ragunayon@gmail.com>
2023-10-27 15:17:28 +0800

    Dev Tools Configuration

--- App.tsx
+++ App.tsx
@@ -1,29 +1,16 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import CounterScreen from './screens/counterScreen';
 
-export default function App() {
-
+const App = () => {
   return (
     <Provider store={store}>
-      {/* <View style={styles.container}>
-        <Text>Counter</Text>
-        <Text>{count} asd</Text>
-        <StatusBar style="auto" />
-      </View> */}
-      <CounterScreen/>
+      <StatusBar style="auto" />
+      <CounterScreen />
     </Provider>
-
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
